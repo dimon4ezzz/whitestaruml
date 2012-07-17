1226f602e37d408f6770dbc70fe1c1d55385a1fc
janszpilewski <janszpilewski@users.sourceforge.net>
2012-07-17 19:04:01 +0000

    C# Parser Core project added

---
+++ staruml/components/parser-core/ParserCore/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using System.Resources;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace ParserCore
+{
+    public class ParserCoreImpl
+    {
+    }
+}
