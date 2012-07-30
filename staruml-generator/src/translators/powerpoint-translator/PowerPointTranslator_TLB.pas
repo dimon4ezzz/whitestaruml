8f0e2e1a09ea087dfba16a281ce0feb3ddce3a3e
janszpilewski <janszpilewski@users.sourceforge.net>
2012-07-30 20:01:25 +0000

    Doc Generators GUIDs updated and all functionality tested

--- staruml-generator/src/translators/powerpoint-translator/PowerPointTranslator_TLB.pas
+++ staruml-generator/src/translators/powerpoint-translator/PowerPointTranslator_TLB.pas
@@ -1,65 +1,65 @@
 unit PowerPointTranslator_TLB;
 
 // ************************************************************************ //
-// WARNING                                                                    
-// -------                                                                    
-// The types declared in this file were generated from data read from a       
-// Type Library. If this type library is explicitly or indirectly (via        
-// another type library referring to this type library) re-imported, or the   
-// 'Refresh' command of the Type Library Editor activated while editing the   
-// Type Library, the contents of this file will be regenerated and all        
-// manual modifications will be lost.                                         
+// WARNING
+// -------
+// The types declared in this file were generated from data read from a
+// Type Library. If this type library is explicitly or indirectly (via
+// another type library referring to this type library) re-imported, or the
+// 'Refresh' command of the Type Library Editor activated while editing the
+// Type Library, the contents of this file will be regenerated and all
+// manual modifications will be lost.
 // ************************************************************************ //
 
-// PASTLWTR : 1.2
-// File generated on 2005-10-26 ¿ÀÈÄ 4:15:47 from Type Library described below.
+// $Rev: 17244 $
+// File generated on 7/30/2012 8:25:03 PM from Type Library described below.
 
 // ************************************************************************  //
-// Type Lib: D:\PLASTIC_CVS\plastic\generator\src\translators\powerpoint-translator\PowerPointTranslator.tlb (1)
-// LIBID: {DB39449E-A29E-4641-8A78-4A597B304D1A}
+// Type Lib: C:\JS\Delphi\WhiteStarSourceforge\staruml-generator\src\translators\powerpoint-translator\PowerPointTranslator (1)
+// LIBID: {AC9DE618-78D1-483A-B543-5C25D203F893}
 // LCID: 0
-// Helpfile: 
+// Helpfile:
 // HelpString: PowerPointTranslator Library
-// DepndLst: 
-//   (1) v2.0 stdole, (C:\WINDOWS\system32\stdole2.tlb)
-//   (2) v1.0 Generator, (C:\Program Files\StarUML\modules\org.staruml.generator\Generator.dll)
+// DepndLst:
+//   (1) v2.0 stdole, (C:\Windows\SysWOW64\stdole2.tlb)
 // ************************************************************************ //
-{$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers. 
+{$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers.
 {$WARN SYMBOL_PLATFORM OFF}
 {$WRITEABLECONST ON}
 {$VARPROPSETTER ON}
+{$ALIGN 4}
 interface
 
-uses Windows, ActiveX, Classes, Generator_TLB, Graphics, StdVCL, Variants;
-  
+uses Windows, ActiveX, Classes, Graphics, OleServer, StdVCL, Variants;
+
 
 // *********************************************************************//
-// GUIDS declared in the TypeLibrary. Following prefixes are used:        
-//   Type Libraries     : LIBID_xxxx                                      
-//   CoClasses          : CLASS_xxxx                                      
-//   DISPInterfaces     : DIID_xxxx                                       
-//   Non-DISP interfaces: IID_xxxx                                        
+// GUIDS declared in the TypeLibrary. Following prefixes are used:
+//   Type Libraries     : LIBID_xxxx
+//   CoClasses          : CLASS_xxxx
+//   DISPInterfaces     : DIID_xxxx
+//   Non-DISP interfaces: IID_xxxx
 // *********************************************************************//
 const
   // TypeLibrary Major and minor versions
   PowerPointTranslatorMajorVersion = 1;
   PowerPointTranslatorMinorVersion = 0;
 
-  LIBID_PowerPointTranslator: TGUID = '{DB39449E-A29E-4641-8A78-4A597B304D1A}';
+  LIBID_PowerPointTranslator: TGUID = '{AC9DE618-78D1-483A-B543-5C25D203F893}';
 
   IID_IPowerPointTranslatorObj: TGUID = '{3AD006D8-8E12-4440-A3DC-BB722EDE5BB2}';
-  CLASS_PowerPointTranslatorObj: TGUID = '{A368F46A-99A0-4F97-BCD7-4404F2AA76DF}';
+  CLASS_PowerPointTranslatorObj: TGUID = '{14E57C1D-24CA-4013-87E4-B5FA373A976D}';
 type
 
 // *********************************************************************//
-// Forward declaration of types defined in TypeLibrary                    
+// Forward declaration of types defined in TypeLibrary
 // *********************************************************************//
   IPowerPointTranslatorObj = interface;
   IPowerPointTranslatorObjDisp = dispinterface;
 
 // *********************************************************************//
-// Declaration of CoClasses defined in Type Library                       
-// (NOTE: Here we map each CoClass to its Default Interface)              
+// Declaration of CoClasses defined in Type Library
+// (NOTE: Here we map each CoClass to its Default Interface)
 // *********************************************************************//
   PowerPointTranslatorObj = IPowerPointTranslatorObj;
 
@@ -83,11 +83,11 @@ type
   end;
 
 // *********************************************************************//
-// The Class CoPowerPointTranslatorObj provides a Create and CreateRemote method to          
-// create instances of the default interface IPowerPointTranslatorObj exposed by              
-// the CoClass PowerPointTranslatorObj. The functions are intended to be used by             
-// clients wishing to automate the CoClass objects exposed by the         
-// server of this typelibrary.                                            
+// The Class CoPowerPointTranslatorObj provides a Create and CreateRemote method to
+// create instances of the default interface IPowerPointTranslatorObj exposed by
+// the CoClass PowerPointTranslatorObj. The functions are intended to be used by
+// clients wishing to automate the CoClass objects exposed by the
+// server of this typelibrary.
 // *********************************************************************//
   CoPowerPointTranslatorObj = class
     class function Create: IPowerPointTranslatorObj;
@@ -109,3 +109,4 @@ begin
 end;
 
 end.
+
