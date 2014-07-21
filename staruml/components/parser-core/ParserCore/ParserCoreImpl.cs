4c406a5b6fffb83000c119a6bb6861ce68366532
janszpilewski <janszpilewski@users.sourceforge.net>
2014-07-21 10:42:21 +0000

    Parser code refactored

--- staruml/components/parser-core/ParserCore/ParserCoreImpl.cs
+++ staruml/components/parser-core/ParserCore/ParserCoreImpl.cs
@@ -8,173 +8,40 @@ using System.Runtime.InteropServices;
 namespace ParserCore
 {
     [ClassInterface(ClassInterfaceType.None)]
-    public abstract class ParserCoreImpl
+    public abstract class ParserCoreImpl : ParserBase.GrammarIndependentParserBase
     {
-        private GOLD.Parser parser = new GOLD.Parser();
-
-        private GOLD.Reduction Root;     //Store the top of the tree
-        private string FailMessage;  // Updated if a parse error occured
-
-        public bool Setup(byte[] grammarTable)
-        {
-            return parser.LoadTables(new BinaryReader(new MemoryStream(grammarTable)));
-        }
-
-        public bool Parse(TextReader reader)
-        {
-
-            GOLD.ParseMessage response;
-            bool done;                      //Controls when we leave the loop
-            bool accepted = false;          //Was the parse successful?
-
-            parser.Open(reader);
-            parser.TrimReductions = false;  //Please read about this feature before enabling  
-
-            done = false;
-            while (!done)
-            {
-                response = parser.Parse();
-
-                switch (response)
-                {
-                    case GOLD.ParseMessage.LexicalError:
-                        //Cannot recognize token
-                        FailMessage = "Lexical Error:\n" +
-                                      "Position: " + parser.CurrentPosition().Line + ", " + parser.CurrentPosition().Column + "\n" +
-                                      "Read: " + parser.CurrentToken().Data;
-                        done = true;
-                        break;
-
-                    case GOLD.ParseMessage.SyntaxError:
-                        //Expecting a different token
-                        FailMessage = "Syntax Error:\n" +
-                                      "Position: " + parser.CurrentPosition().Line + ", " + parser.CurrentPosition().Column + "\n" +
-                                      "Read: " + parser.CurrentToken().Data + "\n" +
-                                      "Expecting: " + parser.ExpectedSymbols().Text();
-                        done = true;
-                        break;
-
-                    case GOLD.ParseMessage.Reduction:
-                        //For this project, we will let the parser build a tree of Reduction objects
-                        //parser.CurrentReduction = CreateNewObject(parser.CurrentReduction);
-                        break;
-
-                    case GOLD.ParseMessage.Accept:
-                        //Accepted!
-                        Root = (GOLD.Reduction)parser.CurrentReduction;    //The root node!                                  
-                        done = true;
-                        accepted = true;
-                        break;
-
-                    case GOLD.ParseMessage.TokenRead:
-                        //You don't have to do anything here.
-                        break;
-
-                    case GOLD.ParseMessage.InternalError:
-                        //INTERNAL ERROR! Something is horribly wrong.
-                        done = true;
-                        break;
-
-                    case GOLD.ParseMessage.NotLoadedError:
-                        //This error occurs if the CGT was not loaded.                   
-                        FailMessage = "Tables not loaded";
-                        done = true;
-                        break;
-
-                    case GOLD.ParseMessage.GroupError:
-                        //GROUP ERROR! Unexpected end of file
-                        FailMessage = "Runaway group";
-                        done = true;
-                        break;
-                }
-            } //while
-
-            return accepted;
-        }
 
         public string GetFailMessage()
         {
-            return FailMessage;
+            return m_failMessage;
         }
 
         public string FindNodeValue(string searchPath)
         {
-            string value = null;
-            string[] searchPathValues = searchPath.Split('.');
+            return ParserBase.ParseTreeSearch.FindNodeValue(searchPath, m_root);
+        } 
 
-            GOLD.Reduction currentNode;
-            if (Root != null)
-                //currentNode = Root[0].Data as GOLD.Reduction; // TODO: Update gramars to skip root production
-                currentNode = Root;
-            else
-                return null; // Cannot continue without search tree
 
-            //int token = 0;
-            bool currentNodeFound = false;
+        private GOLD.Reduction m_root;  // Stores the top of the tree
+        private string m_failMessage;  // Updated if a parsing error occured
 
-            // Iterate over search path nodes
-            for (int i = 0; i < searchPathValues.Length; i++)
-            {
 
-                // Match a tree node with the same name as the specified path node
-                currentNodeFound = false;
-                while (!currentNodeFound) // Guard loop allowing to recover from traversing top level grammar nodes
-                {
-                    for (int j = 0; j < currentNode.Count() && !currentNodeFound; j++)
-                    {
-                        string currentTokenProductionName = currentNode[j].Parent.Name();
-                        if (currentTokenProductionName == searchPathValues[i])
-                        {
-                            GOLD.Reduction iteratedReduction = currentNode[j].Data as GOLD.Reduction;
-                            currentNode = iteratedReduction;
-                            currentNodeFound = true;
-                            break;
-                        }
-                        
-                    } // End of for j
-                    if (!currentNodeFound) // Searched path fragment not found in current production
-                    {
-                        if ((i == 0) && (currentNode.Count() == 1)) // Potentially a dummy top level grammar node, try to skip it
-                            currentNode = currentNode[0].Data as GOLD.Reduction;
-                        else
-                            return null; // Cannot recover, specified path does not exist.
-                    } 
-                } // End of while
-
-            } // End of for i
-
-            if (currentNodeFound)
-            {
-                StringBuilder expressionText = new StringBuilder();
-                collectExpressionText(expressionText, currentNode);
-
-                if (expressionText.Length > 0)
-                    value = expressionText.ToString();
-            
-            }
-  
-            return value;
-        } // End of func FindNodeValue
-
-        private void collectExpressionText(StringBuilder expressionText, GOLD.Reduction reduction)
+        protected override void OnLexicalError()
         {
-            for (int n = 0; n < reduction.Count(); n++)
-            {
-                switch (reduction[n].Type())
-                {
-                    case GOLD.SymbolType.Nonterminal:
-                        GOLD.Reduction subNode = (GOLD.Reduction)reduction[n].Data;
-                        collectExpressionText(expressionText, subNode);
-                        break;
-
-                    default:
-                        string leafText = (string)reduction[n].Data;
-                        expressionText.Append(leafText);
-                        break;
-                }
-            }
+            m_failMessage = String.Format("Lexical Error:\nPosition: {0}, {1}\nRead: {2}", m_coreParser.CurrentPosition().Line,
+                m_coreParser.CurrentPosition().Column, m_coreParser.CurrentToken().Data);
         }
 
+        protected override void OnSyntaxError()
+        {
+            m_failMessage = String.Format("Syntax Error:\nPosition: {0}, {1}\nRead: {2}\nExpecting: {3}", m_coreParser.CurrentPosition().Line,
+                m_coreParser.CurrentPosition().Column, m_coreParser.CurrentToken().Data, m_coreParser.ExpectedSymbols().Text());
+        }
+
+        protected override void OnAccept()
+        {
+            m_root = (GOLD.Reduction)m_coreParser.CurrentReduction;    //The root node! 
+        }
 
 
     } // End of class ParserCoreImpl
