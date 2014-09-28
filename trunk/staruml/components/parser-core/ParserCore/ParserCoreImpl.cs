dad75f8e30621c375e3b7217a06b8f1eb5145dcc
janszpilewski <janszpilewski@a2c42d12-4202-4d66-bf40-0a7c89270c64>
2014-09-28 14:39:58 +0000

    Added Nx grammar parser and validator utility

--- trunk/staruml/components/parser-core/ParserCore/ParserCoreImpl.cs
+++ trunk/staruml/components/parser-core/ParserCore/ParserCoreImpl.cs
@@ -1,47 +1,17 @@
-﻿using System;
-using System.Text;
-using System.IO;
-using System.Resources;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
-namespace ParserCore
+﻿namespace ParserCore
 {
     public abstract class ParserCoreImpl : ParserBase.GrammarIndependentParserBase
     {
 
         public string GetFailMessage()
         {
-            return m_failMessage;
+            return FailMessage;
         }
 
         public string FindNodeValue(string searchPath)
         {
             return ParserBase.ParseTreeSearch.FindNodeValue(searchPath, m_root);
         } 
-
-
-        private GOLD.Reduction m_root;  // Stores the top of the tree
-        private string m_failMessage;  // Updated if a parsing error occured
-
-
-        protected override void OnLexicalError()
-        {
-            m_failMessage = String.Format("Lexical Error:\nPosition: {0}, {1}\nRead: {2}", m_coreParser.CurrentPosition().Line,
-                m_coreParser.CurrentPosition().Column, m_coreParser.CurrentToken().Data);
-        }
-
-        protected override void OnSyntaxError()
-        {
-            m_failMessage = String.Format("Syntax Error:\nPosition: {0}, {1}\nRead: {2}\nExpecting: {3}", m_coreParser.CurrentPosition().Line,
-                m_coreParser.CurrentPosition().Column, m_coreParser.CurrentToken().Data, m_coreParser.ExpectedSymbols().Text());
-        }
-
-        protected override void OnAccept()
-        {
-            m_root = (GOLD.Reduction)m_coreParser.CurrentReduction;    //The root node! 
-        }
-
 
     } // End of class ParserCoreImpl
 }
