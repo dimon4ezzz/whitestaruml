f5a403a0b1e86704cc5505c3b5eb41a041308357
janszpilewski <janszpilewski@users.sourceforge.net>
2016-01-31 18:19:54 +0000

    Cleaned COM registration + spelling issues

--- staruml/components/parser-core/ParserCore/Properties/AssemblyInfo.cs
+++ staruml/components/parser-core/ParserCore/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a5d0740f-ec13-4236-bbc6-9fd1d78bd0aa")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
