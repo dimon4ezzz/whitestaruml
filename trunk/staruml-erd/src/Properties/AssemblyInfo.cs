00d11d5b2a81d2e6b9343cac23f2ea80267c4177
janszpilewski <janszpilewski@a2c42d12-4202-4d66-bf40-0a7c89270c64>
2016-01-31 18:20:28 +0000

    Cleaned COM registration + spelling issues

--- trunk/staruml-erd/src/Properties/AssemblyInfo.cs
+++ trunk/staruml-erd/src/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9ff6d46d-7795-438e-92fe-3ea43c899f46")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
